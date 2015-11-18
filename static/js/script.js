baad6844040f2b2b2a91721e62076b522fdf7900
Lucy Park <me@lucypark.kr>
2015-11-19 08:31:49 +0900

    Remove unused plugin

--- static/js/script.js
+++ static/js/script.js
@@ -65,8 +65,6 @@ $(document).ready(function() {
 			}
 		});
 		
-		//Subscribe
-		new UIMorphingButton( document.querySelector( '.morph-button' ) );
 		// for demo purposes only
 		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
 			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
